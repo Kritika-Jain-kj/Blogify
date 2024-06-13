ee6a4e2f215ae8e05b3e53d923e702744e9e2b1d
Kritika Jain <kritikajain2630@gmail.com>
2024-06-13 11:54:23 +0530

    Rendering Blog

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
-const express = require('express')
+require('dotenv').config()
+
 const path = require('path')
-const app = express()
-const userRoute = require('./routes/user')
+const express = require('express')
 const mongoose = require('mongoose')
 const cookieParser = require('cookie-parser')
+
+const Blog = require('./models/blog')
+
+const userRoute = require('./routes/user')
+const blogRoute = require('./routes/blog')
+
 const { checkForAuthenticationCookie } = require('./middlewares/auth')
+
+const app = express()
+const PORT = process.env.PORT || 8000
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log('MongoDB Connected'))
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
@@ -12,17 +25,17 @@ app.set('views', path.resolve('./views'))
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(checkForAuthenticationCookie('token'))
+app.use(express.static(path.resolve('./public')))
 
-mongoose
-  .connect('mongodb://localhost:27017/blogify')
-  .then(console.log('MongoDB Connected'))
-
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  const allBlogs = await Blog.find({})
   res.render('home', {
     user: req.user,
+    blogs: allBlogs,
   })
 })
 
 app.use('/user', userRoute)
+app.use('/blog', blogRoute)
 
-app.listen(8000, () => console.log('Server Started at PORT 8000!'))
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`))

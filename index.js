d3a16e0d2e03dc6ad6eae3879b23875d303fddad
Kritika Jain <kritikajain2630@gmail.com>
2024-06-13 11:23:27 +0530

    second commit

--- index.js
+++ index.js
@@ -3,20 +3,26 @@ const path = require('path')
 const app = express()
 const userRoute = require('./routes/user')
 const mongoose = require('mongoose')
+const cookieParser = require('cookie-parser')
+const { checkForAuthenticationCookie } = require('./middlewares/auth')
 
 app.set('view engine', 'ejs')
 app.set('views', path.resolve('./views'))
 
 app.use(express.urlencoded({ extended: false }))
+app.use(cookieParser())
+app.use(checkForAuthenticationCookie('token'))
 
 mongoose
   .connect('mongodb://localhost:27017/blogify')
   .then(console.log('MongoDB Connected'))
 
 app.get('/', (req, res) => {
-  res.render('home')
+  res.render('home', {
+    user: req.user,
+  })
 })
 
 app.use('/user', userRoute)
 
-app.listen(8000, () => console.log('Server Started!'))
+app.listen(8000, () => console.log('Server Started at PORT 8000!'))
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const { Schema, model } = require('mongoose')
 const { createHmac, randomBytes } = require('crypto')
+const { createTokenForUser } = require('../services/authentication')
 
 const userSchema = new Schema(
   {
@@ -45,20 +46,24 @@ userSchema.pre('save', function (next) {
   next()
 })
 
-userSchema.static('matchPassword', async function (email, password) {
-  const user = await this.findOne({ email })
-  if (!user) throw new Error('User not found!')
-  const salt = user.salt
-  const hashedPassword = user.password
+userSchema.static(
+  'matchPasswordAndGenerateToken',
+  async function (email, password) {
+    const user = await this.findOne({ email })
+    if (!user) throw new Error('User not found!')
+    const salt = user.salt
+    const hashedPassword = user.password
 
-  const userProvidedHash = createHmac('sha256', salt)
-    .update(user.password)
-    .digest('hex')
-  if (hashedPassword !== userProvidedHash)
-    throw new Error('Incorrect Password!')
+    const userProvidedHash = createHmac('sha256', salt)
+      .update(user.password)
+      .digest('hex')
+    if (hashedPassword !== userProvidedHash)
+      throw new Error('Incorrect Password!')
 
-  return user
-})
+    const token = createTokenForUser(user)
+    return token
+  }
+)
 
 const User = model('user', userSchema)
 
